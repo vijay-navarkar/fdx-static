c77fc0b89b06327f2fd30bc37aced32d10216d32
vijay-navarkar <vijay.a.navarkar@gmail.com>
2020-02-09 20:40:05 +0530

    code commited for eureka client

--- src/main/java/com/fdx/rtct/staticModule/StaticApplication.java
+++ src/main/java/com/fdx/rtct/staticModule/StaticApplication.java
@@ -2,14 +2,13 @@ package com.fdx.rtct.staticModule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @ComponentScan("com.fdx.rtct.staticModule")
 @RequestMapping
 public class StaticApplication {
